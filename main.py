1308f116d831139eaaaa433d9a4c01f92425d914
Yuta <boxodir71777@gmail.com>
2026-07-14 17:34:44 +0500

    refactor stats calculation into function

--- main.py
+++ main.py
@@ -20,30 +20,29 @@ if response.status_code!=200:
     print("API request failed")
     exit()
 
-print(response.status_code)
-
 data=response.json()
-print(json.dumps(data, indent=4))
 
 
+def get_stats(data):
+
+    total_queries=0
+    blocked_queries=0
+
+    for item in data["data"]:
+        if item["status"]=="blocked":
+            blocked_queries=item["queries"]
+
+        if item["status"]=="default":
+            total_queries=item["queries"]
+
+    return total_queries, blocked_queries
 
 
-total_queries = 0
-blocked_queries = 0
-
-
-
-for item in data["data"]:
-    if item["status"] == "blocked":
-        blocked_queries = item["queries"]
-
-    if item["status"] == "default":
-        total_queries = item["queries"]
+total_queries, blocked_queries=get_stats(data)
 
 
 total= total_queries+blocked_queries
 percentage=blocked_queries/total*100
-
 
 
 print(f"Allowed: {total_queries}")
