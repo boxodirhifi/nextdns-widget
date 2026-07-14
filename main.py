d0266207ab8f6976bb2ba3eaed0e28cf9a9f56dc
Yuta <boxodir71777@gmail.com>
2026-07-14 21:25:11 +0500

    add JSON output mode

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from dotenv import load_dotenv
-from datetime import datetime
 from nextdns import get_data
 from stats import get_stats
 from display import show_stats
-import requests
+import argparse
+import json
 import os
 import time
 
@@ -14,15 +14,43 @@ profile_id=os.getenv("NEXTDNS_PROFILE_ID")
 
 
 
+
+def output_json(total_queries,blocked_queries):
+    total = total_queries+blocked_queries
+    percentage=(blocked_queries/total*100) if total else 0
+
+    data={
+        "allowed": total_queries,
+        "blocked": blocked_queries,
+        "total": total,
+        "percentage": round(percentage,2)
+        }
+
+    print(json.dumps(data))
+
+
+
 def main():
+
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--json",action="store_true")
+    args=parser.parse_args()
+
+
     while True:
         data=get_data(api_key,profile_id)
         if data:
             total_queries,blocked_queries=get_stats(data)
-            show_stats(total_queries,blocked_queries)
 
         else:
             print("Unable to fetch NextDNS data")
+
+        if args.json:
+            output_json(total_queries,blocked_queries)
+            return
+
+        else:
+            show_stats(total_queries,blocked_queries)
 
         time.sleep(60)
 
