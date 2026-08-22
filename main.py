6884671f8a959fde1593fc0028a4fea08226ddec
Yuta <boxodir71777@gmail.com>
2026-08-22 21:26:48 +0500

    minor readabitlity changes

--- main.py
+++ main.py
@@ -15,46 +15,46 @@ profile_id=os.getenv("NEXTDNS_PROFILE_ID")
 
 
 
-def output_json(total_queries,blocked_queries):
-    total = total_queries+blocked_queries
-    percentage=(blocked_queries/total*100) if total else 0.0
+def output_json(total_queries: int, blocked_queries: int) -> None:
+    total = total_queries + blocked_queries
+    percentage = (blocked_queries / total * 100) if total else 0.0
 
-    data={
+    data = {
         "allowed": total_queries,
         "blocked": blocked_queries,
         "total": total,
-        "percentage": round(percentage,2)
-        }
+        "percentage": round(percentage, 2)
+    }
 
     print(json.dumps(data))
 
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--json", action="store_true")
+    args = parser.parse_args()
 
-    parser=argparse.ArgumentParser()
-    parser.add_argument("--json",action="store_true")
-    args=parser.parse_args()
-
-
-    total_queries=0
-    blocked_queries=0
+    total_queries = 0
+    blocked_queries = 0
 
     while True:
-        data=get_data(api_key,profile_id)
-        if data:
-            total_queries,blocked_queries=get_stats(data)
-            save_json(total_queries,blocked_queries)
-
-        else:
-            print("Unable to fetch NextDNS data")
+        try:
+            data = get_data(api_key, profile_id)
+            if data:
+                total_queries, blocked_queries = get_stats(data)
+                save_json(total_queries, blocked_queries)
+            else:
+                print("Unable to fetch NextDNS data")
+        except Exception as e:
+            print(f"Error fetching or processing data: {e}")
 
         if args.json:
-            output_json(total_queries,blocked_queries)
+            output_json(total_queries, blocked_queries)
             return
 
         else:
-            show_stats(total_queries,blocked_queries)
+            show_stats(total_queries, blocked_queries)
 
         time.sleep(60)
 
